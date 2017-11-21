1b78548e905f93f4d56ce8c88be7852b6255e96a
student1520142 <shaksed@mail.ru>
2017-11-21 12:46:15 +0300

    add function MyMul()

--- funcs.h
+++ funcs.h
@@ -1,3 +1,5 @@
 #ifndef __FUNCS_H__ #define __FUNCS_H__
-double MySum(double, double); double MySub(double, double);
+double MySum(double, double); 
+double MySub(double, double);
+double MyMul(double, double);
 #endif // __FUNCS_H__
